1ac6df48b4e5dbe92fad952a721d4af06ca9f3a4
Mohanad Bikai <mohanad_bikai@hotmail.com>
2024-11-13 09:36:55 +0100

    git

--- src/data.ts
+++ src/data.ts
@@ -2,5 +2,13 @@ import { Book } from "./book";
 
 export const books: Book[] = [
   { title: "The Awakening", author: "Kate Chopin", id: "1" },
-  { title: "City of Glass", author: "Paul Auster", id: "2" }
+  { title: "City of Glass", author: "Paul Auster", id: "2" },
+  { title: "1984", author: "George Orwell", id: "3" },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", id: "4" },
+  { title: "Pride and Prejudice", author: "Jane Austen", id: "5" },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", id: "6" },
+  { title: "Moby Dick", author: "Herman Melville", id: "7" },
+  { title: "War and Peace", author: "Leo Tolstoy", id: "8" },
+  { title: "The Catcher in the Rye", author: "J.D. Salinger", id: "9" },
+  { title: "Brave New World", author: "Aldous Huxley", id: "10" },
 ];
